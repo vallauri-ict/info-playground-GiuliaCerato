84b8f9525c5ea17fb75f946de63d049828fa8435
GiuliaCerato <71816563+GiuliaCerato@users.noreply.github.com>
2021-02-18 10:44:51 +0100

    Add files via upload

---
+++ EsTriggerCerato/EsTriggerCerato/MSSQLLocalDB.sql
@@ -0,0 +1,43 @@
+﻿CREATE TRIGGER StoricoCancellazioni
+ON Clienti AFTER DELETE
+AS
+BEGIN 
+  DECLARE @currData DATETIME;
+  SET @currData=GETDATE()
+  INSERT INTO StoricoCancellazioni(
+   idCliente, 
+   NomeOld,
+   CognomeOld,
+   CarrelloOld,
+   NomeNew,
+   CognomeNew,
+   CarrelloNew,
+   Data
+  )
+SELECT d.idCLiente, d.Nome, d.Cognome, d.IdCarrello
+FROM deleted d
+
+END
+
+-------------
+
+CREATE TRIGGER StoricoAggiornamenti
+ON Cliente AFTER UPDATE
+AS
+BEGIN
+	DECLARE @currData DATETIME;
+	SET @currData=GETDATE()
+	INSERT INTO StoricoAggiornamenti(
+	idCliente, 
+	NomeOld,
+	CognomeOld,
+	CarrelloOld,
+	NomeNew,
+	CognomeNew,
+	CarrelloNew,
+	Data
+	)
+SELECT d.IdCliente, d.Nome, d.Cognome, d.IdCarrello, i.Nome, i.Cognome, i.IdCarrello, @currData
+FROM deleted d
+
+END
